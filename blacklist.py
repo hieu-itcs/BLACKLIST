5d994f68b20ba101a296f6c18fdb5df6496ecc32
devops-itcs <83263728+devops-itcs@users.noreply.github.com>
2021-07-06 17:17:55 +0700

    Update blacklist.py

--- blacklist.py
+++ blacklist.py
@@ -9,7 +9,6 @@ mbnp = "10.10.99.130"
 vpb = "10.10.150.51"
 vpbbk = "10.10.150.52"
 topcall1 = "10.10.94.34"
-topcall2 = "10.10.90.139"
 topcall3 = "10.10.92.110"
 ops137 = "10.10.99.137"
 
@@ -23,7 +22,7 @@ INSERT INTO userblacklist(prefix,whitelist) VALUES {}\
 '''.format((data,0)))
 	return sql_list
 
-for server in (evn,vtfe,mbnp,vpb,vpbbk,topcall1,topcall2,topcall3,ops137):
+for server in (evn,vtfe,mbnp,vpb,vpbbk,topcall1,topcall3,ops137):
 	conn = psycopg2.connect(
 		database="opensips", 
 		user='opensips', 
@@ -42,6 +41,6 @@ for server in (evn,vtfe,mbnp,vpb,vpbbk,topcall1,topcall2,topcall3,ops137):
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-for server in (evn,vtfe,mbnp,vpb,vpbbk,topcall1,topcall2,topcall3,ops137):
+for server in (evn,vtfe,mbnp,vpb,vpbbk,topcall1,topcall3,ops137):
 	ssh.connect(server, username="root", password="Pls@1234!")
 	stdin,stdout,stderr = ssh.exec_command("opensipsctl fifo reload_blacklist")
